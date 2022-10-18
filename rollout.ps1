6a7b7b439f6f8b6e76b49299f151dcd3cc6b9594
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-10-18 17:58:48 +0200

    add cool rollout file (windows only)

---
+++ rollout.ps1
@@ -0,0 +1,15 @@
+#!/bin/bash
+$git_count=$(git rev-list --count master)
+
+echo "Rolling out +$git_count ..."
+
+echo "Cleaning.."
+flutter clean && flutter pub get
+
+echo "Building for Android.."
+flutter build appbundle --release --build-number="$git_count"
+echo "Fastlane android.."
+cd android && bundle exec fastlane beta && cd ..
+echo "Cleaning.."
+flutter clean && flutter pub get
+echo "Done"
