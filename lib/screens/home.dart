b0ef4fe420190762c75a7baa0432d4ebc0b5ddb3
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-01-15 15:21:13 +0100

    safe area for ios users mainly

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> {
           children: const [
             GMap(),
             TopHeader(),
-            MapInfoSheet(),
+            SafeArea(child: MapInfoSheet()),
           ],
         ),
       ),
