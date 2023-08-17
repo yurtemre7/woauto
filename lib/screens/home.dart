b8d30d3c69ead049247ec432aecb850f78faa1fd
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-08-17 19:00:49 +0200

    fix keep screen on

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -132,7 +132,7 @@ class _HomeState extends State<Home> {
             selectedIndex: woAuto.currentIndex.value,
             onDestinationSelected: (index) {
               woAuto.currentIndex.value = index;
-              if (index == 1) {
+              if (woAuto.drivingMode.value) {
                 KeepScreenOn.turnOn();
               } else {
                 KeepScreenOn.turnOff();
