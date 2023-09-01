3c4ce57f76dbd08783b2cb708d2cc40f63afa7ae
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-09-01 20:08:31 +0200

    remove

--- lib/components/top_header.dart
+++ lib/components/top_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/scheduler/ticker.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:maps_launcher/maps_launcher.dart';
