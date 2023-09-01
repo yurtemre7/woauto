7c838d99057c3fcd313e716dcde292ae884651aa
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-09-01 14:44:27 +0200

    new car class that has all attributes

--- lib/classes/car_location.dart
+++ lib/classes/car_location.dart
@@ -4,8 +4,18 @@ class CardLocation {
   String long;
   String name;
   String createdAt;
+  String view = '';
+  String accountId = '';
 
-  CardLocation(this.id, this.lat, this.long, this.name, this.createdAt);
+  CardLocation(
+    this.id,
+    this.lat,
+    this.long,
+    this.name,
+    this.createdAt, {
+    this.view = '',
+    this.accountId = '',
+  });
 
   factory CardLocation.fromJson(Map<String, dynamic> json) {
     var location = json['msg'];
@@ -18,9 +28,22 @@ class CardLocation {
     );
   }
 
+  factory CardLocation.fromSPJson(Map<String, dynamic> json) {
+    var location = json['msg'];
+    return CardLocation(
+      location[0].toString(),
+      location[1].toString(),
+      location[2].toString(),
+      location[3].toString(),
+      location[4].toString(),
+      view: location[5] != null ? location[5].toString() : '',
+      accountId: location[6] != null ? location[6].toString() : '',
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
-      'msg': [id, name, lat, long, createdAt],
+      'msg': [id, name, lat, long, createdAt, view, accountId],
     };
   }
 }
---
+++ lib/classes/car_park.dart
@@ -0,0 +1,74 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CarPark {
+  final String uuid;
+  String name;
+  int? createdAt;
+  int? updatedAt;
+  double latitude;
+  double longitude;
+  String? adresse;
+  bool sharing;
+  String? description;
+  String? photoPath;
+
+  String viewKey = '';
+  String editKey = '';
+  int? until;
+
+  CarPark({
+    required this.uuid,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    this.adresse,
+    this.createdAt,
+    this.updatedAt,
+    this.sharing = false,
+    this.description,
+    this.photoPath,
+    this.viewKey = '',
+    this.editKey = '',
+    this.until,
+  });
+
+  factory CarPark.fromJson(Map<String, dynamic> json) {
+    return CarPark(
+      uuid: json['uuid'],
+      name: json['name'] ?? 'Mein Auto',
+      latitude: json['latitude'] ?? 0.0,
+      longitude: json['longitude'] ?? 0.0,
+      adresse: json['adresse'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+      sharing: json['sharing'] ?? false,
+      description: json['description'],
+      photoPath: json['photoPath'],
+      viewKey: json['viewKey'] ?? '',
+      editKey: json['editKey'] ?? '',
+      until: json['until'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'uuid': uuid,
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+      'adresse': adresse,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'sharing': sharing,
+      'description': description,
+      'photoPath': photoPath,
+      'viewKey': viewKey,
+      'editKey': editKey,
+      'until': until,
+    };
+  }
+
+  LatLng get latLng => LatLng(latitude, longitude);
+
+  bool get mine => editKey.isNotEmpty;
+}
