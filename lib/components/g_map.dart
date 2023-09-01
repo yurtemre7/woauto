a9f50b86f4aefc1c9d95c1e4510a680ab84c310c
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-09-01 15:05:36 +0200

    remove

--- lib/components/g_map.dart
+++ lib/components/g_map.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:expandable/expandable.dart';
 import 'package:flutter/foundation.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
