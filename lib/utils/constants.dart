4d6abc3f5c70360ea572da0035d7ef2dece1b657
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-09-01 14:43:47 +0200

    add new constant

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
 const String scheme = 'https://';
 const String host = 'woauto.yurtemre.de';
 
 const String createPath = '/create/';
 const String updatePath = '/update/';
 const String getPath = '/';
+
+const double CAM_ZOOM = 18;
