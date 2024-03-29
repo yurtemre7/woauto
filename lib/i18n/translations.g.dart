477bcbd336919878a5f027e9a9a532329d686014
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-03-29 13:16:55 +0100

    update

--- lib/i18n/translations.g.dart
+++ lib/i18n/translations.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 432 (216 per locale)
 ///
-/// Built on 2024-03-29 at 12:12 UTC
+/// Built on 2024-03-29 at 12:16 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
