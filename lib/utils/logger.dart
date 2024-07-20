b6bd1313f04394ae8d1a0c295ca4ed00a36876f0
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-07-20 11:04:20 +0200

    improve method return type

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 
-logMessage(String message, {String? tag}) {
+void logMessage(String message, {String? tag}) {
   if (!kDebugMode) return;
   log('${tag ?? 'WoAuto'}: $message');
 }
