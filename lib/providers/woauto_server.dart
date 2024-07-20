b6747d6ee58475fa1dc33d2dec42be8863e7c48c
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-07-20 13:39:43 +0200

    improve fetching of friends pos & parkings + subscribe to live changes

--- lib/providers/woauto_server.dart
+++ lib/providers/woauto_server.dart
@@ -1,42 +1,51 @@
 import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:woauto/classes/account.dart';
-import 'package:woauto/classes/car_location.dart';
 import 'package:woauto/classes/car_park.dart';
+import 'package:woauto/classes/wa_position.dart';
+import 'package:woauto/classes/wa_simple_position.dart';
 import 'package:woauto/main.dart';
 import 'package:woauto/utils/constants.dart';
 import 'package:woauto/utils/logger.dart';
+import 'package:woauto/utils/utilities.dart';
 
 class WoAutoServer extends GetxController {
   /// Our PocketBase instance
   late PocketBase pb;
   var httpClient = GetHttpClient(baseUrl: '$scheme$host:$port');
 
-  var locations = <String, CardLocation>{}.obs;
-  var accounts = <String, Account>{}.obs;
-
   var serverWorks = false.obs;
+
+  // user data
+  /// Only while app usage, will this be "live", else it will be the last location.
+  var shareMyLastLiveLocation = false.obs;
+
+  /// Thoses are your car parkings
+  var shareMyParkings = false.obs;
+
+  @override
+  onClose() async {
+    logMessage('Disposing WoAuto Server');
+    await pb.realtime.unsubscribe();
+    logMessage('Unsubscribed from all real-time subscriptions');
+    super.onClose();
+  }
 
   toJson() {
     return json.encode({
-      'locations': locations.values.map((e) => e.toJson()).toList(),
-      'accounts': accounts.values.map((e) => e.toJson()).toList(),
+      'share_me': shareMyLastLiveLocation.value,
+      'share_parkings': shareMyParkings.value,
     });
   }
 
   static WoAutoServer fromJson(String jsonString) {
     var jsonMap = json.decode(jsonString);
     var server = WoAutoServer();
-    jsonMap['locations'].forEach((e) {
-      var location = CardLocation.fromJson(e);
-      server.locations[location.id] = location;
-    });
-    jsonMap['accounts'].forEach((e) {
-      var account = Account.fromJson(e);
-      server.accounts[account.id] = account;
-    });
+
+    server.shareMyLastLiveLocation.value = jsonMap['share_me'] ?? false;
+    server.shareMyParkings.value = jsonMap['share_parkings'] ?? false;
     return server;
   }
 
@@ -68,116 +77,167 @@ class WoAutoServer extends GetxController {
       server.serverWorks.value = true;
     }
 
+    var user =
+        await server.getUser(expand: 'friends,friends.position,friends.parkings,parkings,position');
+
+    if (user == null) return server;
+
+    var friends = user.expand['friends'];
+    if (friends != null) {
+      for (var friend in friends) {
+        var fPosition = friend.expand['position']?.first;
+        if (fPosition != null) {
+          logMessage('Position: ${fPosition.data}', tag: friend.id);
+          var simplePosition = WaSimplePosition.fromRecord(fPosition);
+          woAuto.addFriendPosition(
+            newPosition: simplePosition.latLng,
+            uuid: simplePosition.id,
+            newName: friend.data['username'],
+          );
+        }
+        var fParkings = friend.expand['parkings'];
+        if (fParkings != null) {
+          for (var fPark in fParkings) {
+            logMessage('Parking ${fPark.id}: ${fPark.data}', tag: friend.id);
+            var fPosition = WaPosition.fromRecord(fPark);
+            woAuto.addFriendPosition(
+              newPosition: fPosition.latLng,
+              uuid: fPosition.id,
+              newName: fPosition.name,
+            );
+          }
+        }
+
+        server.pb.collection('users').subscribe(
+          friend.id,
+          expand: 'friends,friends.position,friends.parkings,parkings,position',
+          (userData) async {
+            var fUser = userData.record;
+            if (fUser == null) return;
+            logMessage('Friend updated', tag: fUser.id);
+            var fPosition = fUser.expand['position']?.first;
+            if (fPosition != null) {
+              logMessage('Position: ${fPosition.data}', tag: fUser.id);
+              var simplePosition = WaSimplePosition.fromRecord(fPosition);
+              woAuto.addFriendPosition(
+                newPosition: simplePosition.latLng,
+                uuid: simplePosition.id,
+                newName: friend.data['username'],
+              );
+            }
+            var fParkings = friend.expand['parkings'];
+            if (fParkings != null) {
+              for (var fPark in fParkings) {
+                logMessage('Parking ${fPark.id}: ${fPark.data}', tag: fUser.id);
+                var fPosition = WaPosition.fromRecord(fPark);
+                woAuto.addFriendPosition(
+                  newPosition: fPosition.latLng,
+                  uuid: fPosition.id,
+                  newName: fPosition.name,
+                );
+              }
+            }
+          },
+        );
+      }
+    }
     return server;
   }
 
   reset() {
-    locations.clear();
-    accounts.clear();
+    shareMyLastLiveLocation.value = false;
+    shareMyParkings.value = false;
     save();
   }
 
-  Future<Account?> createLocation(CarPark park, String until) async {
+  Future<RecordModel?> getUser({String? id, String? expand}) async {
+    RecordModel user;
+    var localUser = pb.authStore.model as RecordModel?;
+    if (localUser == null) return null;
+    id ??= localUser.id;
+    user = await pb.collection('users').getOne(id, expand: expand);
+    logMessage('Fetched User $id: ${user.data}');
+    if (expand != null) {
+      // logMessage('User $id expanded: ${user.expand}');
+    }
+    return user;
+  }
+
+  Future<void> setUserLocation(LatLng position) async {
     try {
-      var response = await httpClient.post(
-        createPath,
-        query: {
-          'uuid': park.uuid,
-          'name': park.name,
-          'lat': park.latitude.toString(),
-          'long': park.longitude.toString(),
-          'until': until,
-        },
-      );
-      var jsonMap = json.decode(response.body);
-      if (jsonMap['msg'] is! List) {
-        return null;
+      // example create body
+      var user = pb.authStore.model as RecordModel?;
+      if (user == null) return;
+      // var user = await pb.collection('users').getOne(userOld.id);
+      var positionData = user.data['position'].toString().trim();
+      // debugPrint(user.data.toString());
+      RecordModel record;
+      var body = <String, dynamic>{
+        'latitude': position.latitude,
+        'longitude': position.longitude,
+        'user': user.id,
+      };
+      if (positionData.isEmpty) {
+        record = await pb.collection('simple_position').create(body: body);
+      } else {
+        record = await pb.collection('simple_position').update(positionData, body: body);
       }
-      Account account = Account.fromJson(jsonMap);
-      save();
-      serverWorks.value = true;
-      return account;
-    } catch (e) {
-      logMessage('Could not create location: $e');
-      serverWorks.value = false;
-      return null;
+
+      await pb.collection('users').update(user.id, body: {
+        'position': record.id,
+      });
+    } on ClientException catch (e) {
+      var code = e.response['code'];
+      var message = e.response['message'];
+      logMessage('Fehler $code:\n$message');
     }
   }
 
-  Future<Account?> updateLocation({required CarPark park}) async {
+  Future<void> deleteUserLocation() async {
     try {
-      var response = await httpClient.post(
-        updatePath,
-        query: {
-          'name': park.name,
-          'lat': park.latitude.toString(),
-          'long': park.longitude.toString(),
-          'uuid': park.uuid,
-          'edit': park.editKey,
-        },
-      );
-      var jsonMap = json.decode(response.body);
-      if (jsonMap['msg'] is! List) {
-        return null;
+      // example create body
+      var user = pb.authStore.model as RecordModel;
+      // var user = await pb.collection('users').getOne(userOld.id);
+      var positionData = user.data['position'].toString().trim();
+      // debugPrint(user.data.toString());
+      if (positionData.isEmpty) {
+        return;
+      } else {
+        await pb.collection('simple_position').delete(positionData);
       }
-      Account account = Account.fromJson(jsonMap);
-      save();
-      serverWorks.value = true;
-      return account;
-    } catch (e) {
-      logMessage('Could not update location: $e');
-      serverWorks.value = false;
-      return null;
+
+      await pb.collection('users').update(user.id, body: {
+        'position': null,
+      });
+    } on ClientException catch (e) {
+      var code = e.response['code'];
+      var message = e.response['message'];
+      logMessage('Fehler $code:\n$message');
     }
   }
 
-  Future<bool?> deleteLocationAccount({required CarPark park}) async {
+  Future<void> deleteUserParkingLocations() async {
     try {
-      var response = await httpClient.post(
-        deletePath,
-        query: {
-          'uuid': park.uuid,
-          'edit': park.editKey,
-        },
-      );
-      var jsonMap = json.decode(response.body);
-      if (jsonMap['msg'] == null) {
-        return null;
+      // example create body
+      var user = pb.authStore.model as RecordModel;
+      // var user = await pb.collection('users').getOne(userOld.id);
+      var parkingsData = user.data['parkings'] as List<String>;
+      // debugPrint(user.data.toString());
+      if (parkingsData.isEmpty) {
+        return;
+      } else {
+        for (var parkingId in parkingsData) {
+          await pb.collection('positions').delete(parkingId);
+        }
       }
 
-      save();
-      serverWorks.value = true;
-      return jsonMap['msg'] == 'deleted';
-    } catch (e) {
-      logMessage('Could not delete location: $e');
-      serverWorks.value = false;
-      return null;
-    }
-  }
-
-  Future<CardLocation?> getLocation({required String id, required String view}) async {
-    try {
-      var response = await httpClient.get(
-        getPath,
-        query: {
-          'uuid': id,
-          'view': view,
-        },
-      );
-      var jsonMap = json.decode(response.body);
-      if (jsonMap['msg'] is! List) {
-        return null;
-      }
-      CardLocation location = CardLocation.fromJson(jsonMap);
-      location.view = view;
-      location.accountId = id;
-      save();
-      serverWorks.value = true;
-      return location;
-    } catch (e) {
-      logMessage('Could not get location: $e');
-      serverWorks.value = false;
-      return null;
+      await pb.collection('users').update(user.id, body: {
+        'parkings': null,
+      });
+    } on ClientException catch (e) {
+      var code = e.response['code'];
+      var message = e.response['message'];
+      logMessage('Fehler $code:\n$message');
     }
   }
 }
