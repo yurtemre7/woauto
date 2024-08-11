2e6edced251425fdcbe2140ab1c5df4e87798a29
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-11 19:09:07 +0200

    update packages & new flutter version

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import GoogleMaps
 import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
