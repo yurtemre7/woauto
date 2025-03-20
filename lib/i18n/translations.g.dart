7f1998ca8541be21101be576d9e0382cbb9f2ecc
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-03-20 22:56:49 +0100

    new slang

--- lib/i18n/translations.g.dart
+++ lib/i18n/translations.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 488 (244 per locale)
 ///
-/// Built on 2024-12-17 at 21:18 UTC
+/// Built on 2025-03-20 at 21:55 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
@@ -32,8 +32,8 @@ enum AppLocale with BaseAppLocale<AppLocale, Translations> {
 
 	const AppLocale({
 		required this.languageCode,
-		this.scriptCode, // ignore: unused_element
-		this.countryCode, // ignore: unused_element
+		this.scriptCode, // ignore: unused_element, unused_element_parameter
+		this.countryCode, // ignore: unused_element, unused_element_parameter
 	});
 
 	@override final String languageCode;
