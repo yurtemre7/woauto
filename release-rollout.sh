db5f1c00590370add150565009fa02d36f2932fc
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-07-02 19:47:04 +0200

    feat: add release rollout script for Android builds

---
+++ release-rollout.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git_count=$(git rev-list --count master)
+
+echo "Rolling out +$git_count ..."
+
+read -p "Press enter to continue..."
+
+echo "Cleaning.."
+flutter clean && flutter pub get
+
+echo "Building for Android.."
+flutter build appbundle --release --build-number="$git_count"
+echo "Fastlane android.."
+cd android && fastlane release && cd ..
+echo "Cleaning.."
+flutter clean && flutter pub get
+echo "Done"
